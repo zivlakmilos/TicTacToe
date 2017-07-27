0b75de4f0d8e6f0aa7cefb31d71c23ebdd283d36
Milos Zivlak <zivlakmilos@gmail.com>
2017-07-27 13:31:04 +0200

    Added screen interface (for game screen managing)

---
+++ include/screen.h
@@ -0,0 +1,19 @@
+#ifndef _SCREEN_H_
+#define _SCREEN_H_
+
+#include <SFML/Graphics/RenderWindow.hpp>
+
+class Screen
+{
+public:
+    Screen(void) {}
+    virtual Screen(void) {}
+
+    virtual void handleEvents(void) = 0;
+    virtual void update(float deltaTime) = 0;
+    virtual void draw(sf::RenderWindow &window) = 0;
+
+private:
+};
+
+#endif // _SCREEN_H_
